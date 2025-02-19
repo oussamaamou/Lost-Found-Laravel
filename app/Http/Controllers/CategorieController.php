93066df6570973ab3862269143eea6635a7a79f5
oussamaamou <amoussdark@gmail.com>
2025-02-19 14:00:15 +0100

    Creation du controlleur de model categorie

--- app/Http/Controllers/CategorieController.php
+++ app/Http/Controllers/CategorieController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categorie;
 use Illuminate\Http\Request;
 
 class CategorieController extends Controller
 {
-    //
+    public function getAllCategories(){
+
+        $categories = Categorie::all();
+        return view('dashboard', ['categories' => $categories]);
+    }
 }

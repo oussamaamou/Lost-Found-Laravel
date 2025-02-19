c714280827034f6934384393e392c60f4dcf9a8d
oussamaamou <amoussdark@gmail.com>
2025-02-19 14:08:18 +0100

    Ajout d'une colonne

---
+++ database/migrations/2025_02_18_233808_add_poste_id_to_commentaires_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('commentaires', function (Blueprint $table) {
+            $table->unsignedBigInteger('poste_id')->after('id'); 
+            $table->foreign('poste_id')->references('id')->on('postes')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('commentaires', function (Blueprint $table) {
+            $table->dropForeign(['poste_id']);
+            $table->dropColumn('poste_id'); 
+        });
+    }
+};

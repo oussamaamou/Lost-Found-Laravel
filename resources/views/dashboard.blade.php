106c15c71669687c113726a3d3020f3173c37e49
oussamaamou <amoussdark@gmail.com>
2025-02-19 14:03:44 +0100

    Modifiacation des vues d'affichage des postes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,6 +5,11 @@
         </h2>
     </x-slot>
 
+    <!-- Lien du Tailwind -->
+    <script src="https://cdn.tailwindcss.com"></script>
+     <!-- Lien des Icons -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" /> 
+ 
     
 
     <!-- ///////////////////////////////////////////////////////////////////////////// -->
@@ -16,7 +21,12 @@
                 <h1 class="text-xl mt-[-2rem] font-bold leading-tight tracking-tight text-stone-700 md:text-2xl dark:text-white">
                     Publier votre Cours 
                 </h1>
-                <form class="space-y-1" method="POST" enctype="multipart/form-data">
+                <form action="{{route('dashboard.create')}}" class="space-y-1" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('post')
+
+                    <input type="hidden" name="users_id" id="users_id" value="{{ Auth::user()->id }}">
+
                     <div>
                         <label for="titre" class="block mb-2 text-sm font-medium text-stone-700 dark:text-white">Titre</label>
                         <input type="text" name="titre" id="titre" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" >
@@ -29,33 +39,33 @@
                     <div class="grid gap-6 mb-6 md:grid-cols-2">
 
                         <div>
-                            <label for="type" class="block mb-2 text-sm font-medium text-stone-700 dark:text-white">Categorie </label>
-                            <select id="type" name="type" class="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                <option value="1">Téléphone</option>
+                            <label for="categorie_id" class="block mb-2 text-sm font-medium text-stone-700 dark:text-white">Categorie </label>
+                            <select id="categorie_id" name="categorie_id" class="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option value="1">Telephone</option>
                                 <option value="2">Document</option>
                             </select>
                         </div>
 
                         <div>
-                            <label for="titre" class="block mb-2 text-sm font-medium text-stone-700 dark:text-white">Téléphone</label>
-                            <input type="text" name="titre" id="titre" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" >
+                            <label for="telephone" class="block mb-2 text-sm font-medium text-stone-700 dark:text-white">Téléphone</label>
+                            <input type="text" name="telephone" id="telephone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" >
                         </div>
 
                     </div>
                         
                     <div>
-                        <label for="titre" class="block mb-2 text-sm font-medium text-stone-700 dark:text-white">Email</label>
-                        <input type="text" name="titre" id="titre" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" >
+                        <label for="email" class="block mb-2 text-sm font-medium text-stone-700 dark:text-white">Email</label>
+                        <input type="text" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" >
                     </div>
 
                     <div class="mb-5">
-                        <label for="date_reservation" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date d'Action</label>
-                        <input type="datetime-local" id="date_reservation" name="date_reservation" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+                        <label for="dateAction" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date d'Action</label>
+                        <input type="datetime-local" id="dateAction" name="dateAction" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
                     </div>
 
                     <div>
-                        <label for="contenu" class="block mb-2 text-sm font-medium text-stone-700 dark:text-white">Lieu d'Action</label>
-                        <input type="text" id="contenu" name="contenu" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" >
+                        <label for="lieuAction" class="block mb-2 text-sm font-medium text-stone-700 dark:text-white">Lieu d'Action</label>
+                        <input type="text" id="lieuAction" name="lieuAction" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" >
                     </div>
                     
                     <button type="submit" class="ml-[7rem] mt-[5rem] w-[8rem] text-white bg-gradient-to-r from-orange-400 via-orange-500 to-orange-600 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Creer</button>
@@ -79,41 +89,63 @@
         
         
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">   
-            
+
+
+
+        @foreach ($postes as $poste)    
         <!-- ////////////////////////////////////////////////////////////////////////////////////////////////////// -->
             <div class="mt-[4rem] ml-[10.5rem] bg-white dark:bg-gray-800 rounded-xl shadow-2xl max-w-4xl w-full p-8 transition-all duration-300 animate-fade-in pt-[3.5rem]">
                 <div class="flex flex-col md:flex-row">
                     <div class="md:w-1/3 text-center mb-8 md:mb-0">
                         <img src="https://st5.depositphotos.com/17433220/73304/i/450/depositphotos_733041078-stock-photo-silhouette-adult-man-male-avatar.jpg" alt="Profile Picture" class="rounded-full w-48 h-48 mx-auto mb-4 border-4 border-orange-500 transition-transform duration-300 hover:scale-105">
                         <h1 class="text-2xl font-bold text-orange-500 dark:text-white mb-2"></h1>
-                        <p class="text-stone-700 font-semibold">Ahmed </p>
+                        <p class="text-stone-700 font-semibold">{{ Auth::user()->name }}</p>
                 
 
-                        <h2 class="text-xl font-semibold text-orange-500 mb-4  mt-[3rem]">Catégorie</h2>
-                        <p class="text-stone-700 font-semibold">Ahmed</p>
+                        <h2 class="text-xl font-bold text-orange-500 mb-4  mt-[3rem]">Contact</h2>
+                        <p class="text-stone-700 mb-4 text-left font-semibold"> <span class="text-orange-500 font-bold">Email</span> : {{ $poste->email }} </p>
+                        <p class="text-stone-700 text-left font-semibold"> <span  class="text-orange-500 font-bold">Téléphone</span> : {{ $poste->telephone }} </p>
 
-                
-                        <button class="ml-[5.7rem] mt-[5rem] flex items-center rounded-md border border-orange-300 py-2 px-4 text-center text-sm transition-all shadow-sm hover:shadow-lg text-orange-600 hover:text-white hover:bg-orange-800 hover:border-orange-800 focus:text-white focus:bg-orange-800 focus:border-orange-800 active:border-orange-800 active:text-white active:bg-orange-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
-                            Details
-                            
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 ml-1.5">
-                                <path fill-rule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clip-rule="evenodd" />
-                            </svg>
-                        </button>
-                
+                        <div class="grid mt-8 grid-cols-2">
+                            <div>
+                                <button id="ajtpost" type="button" class="ml-[0rem] text-white bg-yellow-300 hover:bg-yellow-400 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                    </svg>
+
+                                </button>
+                            </div>
+                        
+                            <form id="" method="POST">
+                                <input type="hidden" name="supprimer_cours" value="1">
+
+                                <button class="text-white bg-red-500 hover:bg-red-600 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-red-900">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                    </svg>
+
+
+                                </button>
+                            </form>
+                        </div>
 
                     </div>
+
                     <div class="md:w-2/3 md:pl-8">
-                        <p class="text-gray-700 font-semibold text-2xl dark:text-gray-300 mb-6"> Ahmed</p>
+                        <p class="text-gray-700 font-semibold text-2xl dark:text-gray-300 mb-6"> {{ $poste->titre }}</p>
                         <div class="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg lg:overflow-visible">
+                        <img class="object-fit object-center w-full h-96" src="https://d2u4q3iydaupsp.cloudfront.net/k5GdinclmQaklFxCIZxP3piLQokCg87uXpmu5bs4MjIdbJno9lYsI2gvqJJKb0seEq5mqhstxZCKennqUdTKgdzlULeXRsEKDNVE5JgKvcN3DqcCQneLY1b213iaBNM8" alt="nature image"/>
                         </div>
                         <p class="text-gray-700 dark:text-gray-300 mb-6 mt-6">
+                            {{ $poste->description }}
                         </p>
                     </div>
                 </div>
 
             </div>
+
         <!-- ////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+        @endforeach
             
                 
         </div>

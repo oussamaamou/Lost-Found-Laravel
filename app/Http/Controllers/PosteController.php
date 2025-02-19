cdba1637c31b22d74dc5df3a9fdc573c5ce4f796
oussamaamou <amoussdark@gmail.com>
2025-02-19 03:34:04 +0100

    Ajout du poste controlleur

--- app/Http/Controllers/PosteController.php
+++ app/Http/Controllers/PosteController.php
@@ -3,12 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Postes;
 
 class PosteController extends Controller
 {
     //
-    public function getAllPoste(){
+    public function getAllPostes(){
         return view('postes.index');
         
+    }
+
+    public function getMyPostes(){
+
+        $postes = Postes::all();
+        return view('dashboard', ['postes' => $postes]);
+    }
+
+    public function create(Request $request){
+
+        $data =  $request->validate([
+
+            'titre' => 'required|string',
+            'description' => 'required|string',
+            'categorie_id' => 'required|numeric',
+            'telephone' => 'required|string',
+            'email' => 'required|email',
+            'dateAction' => 'required|date',
+            'lieuAction' => 'required|string',
+            'users_id' => 'required|numeric'
+            
+        ]);
+
+        $newPoste = Postes::create($data);
+        return redirect(route('dashboard'));
     }
 }

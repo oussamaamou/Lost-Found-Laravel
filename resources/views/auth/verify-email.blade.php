d74337dcde0a2c68990ea1a5a7eda1882d689911
oussamaamou <amoussdark@gmail.com>
2025-02-19 14:03:06 +0100

    Modifiacation des vues d'authentification

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -3,6 +3,9 @@
         {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
     </div>
 
+    <!-- Lien du Tailwind -->
+    <script src="https://cdn.tailwindcss.com"></script>
+
     @if (session('status') == 'verification-link-sent')
         <div class="mb-4 font-medium text-sm text-green-600">
             {{ __('A new verification link has been sent to the email address you provided during registration.') }}

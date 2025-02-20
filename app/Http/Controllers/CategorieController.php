aa192ae5a9bd3ad82254578612d4187320b98243
oussamaamou <amoussdark@gmail.com>
2025-02-20 16:53:20 +0100

    Modification du categorie controlleur

--- app/Http/Controllers/CategorieController.php
+++ app/Http/Controllers/CategorieController.php
@@ -5,11 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Categorie;
 use Illuminate\Http\Request;
 
-class CategorieController extends Controller
-{
-    public function getAllCategories(){
-
-        $categories = Categorie::all();
-        return view('dashboard', ['categories' => $categories]);
-    }
+class CategorieController extends Controller{
+    
 }

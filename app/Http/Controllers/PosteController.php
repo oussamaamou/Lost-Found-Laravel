bf67f7d47756a6ecd2d29634d020069667581355
oussamaamou <amoussdark@gmail.com>
2025-02-21 10:18:19 +0100

    Ajout des methodes de filtrage et de recherche

--- app/Http/Controllers/PosteController.php
+++ app/Http/Controllers/PosteController.php
@@ -6,17 +6,21 @@ use Illuminate\Http\Request;
 use App\Models\Postes;
 use App\Models\Categorie;
 
+use function Laravel\Prompts\search;
+
 class PosteController extends Controller
 {
-    //
+    
     public function getAllPostes(){
-        return view('postes.index');
+        $postes = Postes::all();
+        $categories = Categorie::all();
+        return view('postes.index', ['postes' => $postes], ['categories' => $categories]);
         
     }
 
     public function getMyPostes(){
 
-        $postes = Postes::where('users_id', auth()->id())->get();
+        $postes = Postes::where('users_id', auth()->id())->with('user')->get();
         $categories = Categorie::all();
         return view('dashboard', ['postes' => $postes], ['categories' => $categories]);
     }
@@ -71,6 +75,40 @@ class PosteController extends Controller
         
         $poste->delete();
         return redirect(route('dashboard'));
+
+    }
+
+    public function search(Request $request){
+
+        $search = $request->search;
+        $categories = Categorie::all();
+
+        $postes = Postes::where(function($query) use ($search) {
+            $query->where('titre', 'like', "%$search%")
+            ->orWhere('description', 'like', "%$search%");
+        })
+
+        ->orWhereHas('user', function($query) use ($search){
+            $query->where('name', 'like', "%$search%");
+        })
+
+        ->get();
+
+        return view('postes.index', compact('postes','search'), ['categories' => $categories]);
+    }
+
+    public function filterByCategorie(Request $request){
+
+        $filter = $request->filter;
+        $categories = Categorie::all();
+
+
+        $postes = Postes::where(function($query) use ($filter){
+            $query->where('categorie_id', 'like', "%$filter%");
+        })
+        ->get();
+
+        return view('postes.index', compact('postes','filter'), ['categories' => $categories]);
 
     }
 
